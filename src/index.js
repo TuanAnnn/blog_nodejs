57b213bd1060f2734641db4a6c334b374d06addf
TuanAn <tuananthptcc10a5@gmail.com>
2023-06-12 16:44:01 +0700

    create routes and connect mongodb

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const morgan = require('morgan')
 const {engine} =  require('express-handlebars');
 const app = express();
 const port = 3000;
+const route = require('./routers')
+
+
+const db = require('./config/db') 
+
+// Connect db
+
+db.connect()
 
 
 //confict static
@@ -27,19 +35,9 @@ app.set('view engine', 'hbs');
 
 app.set('views', path.join(__dirname,'resource/views'));
 
-app.get('/', (req, res) => {
-  res.render('home')
-});
-app.get('/news', (req, res) => {
-  res.render('news')
-});
-app.get('/search', (req, res) => {
-  res.render('search')
-});
-app.post('/search', (req, res) => {
-  console.log(req.body)
-  res.render('search')
-});
+
+// Route init
+route(app)
 // console.log('PATH',path.join(__dirname,'resource','views'))
 app.listen(port);
 
