abd7124dc339f1839598518da9bc8192925ba384
TuanAn <tuananthptcc10a5@gmail.com>
2023-06-13 11:53:02 +0700

    read data from db and build new UI for home page

--- src/app/controllers/SiteControllers.js
+++ src/app/controllers/SiteControllers.js
@@ -1,13 +1,16 @@
 const Course = require("../models/Course");
-
+const {multiMongooseToObject} = require('../../utils/mongoose')
 class SiteControllers {
   //[GET] /
   index(req, res) {
-    Course.find({}).then(
-        courses => res.json(courses)
-    ).catch(
-        err=>res.status(400).json({err:'error'})
-    )
+    Course.find({})
+      .then(courses =>{
+        res.render("home", {
+          courses:multiMongooseToObject(courses)
+        })
+      }
+      )
+      .catch((err) => res.status(400).json({ err: "error" }));
     // res.render('home')
   }
 
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
   name: { type: String },
-  age: { type: Number },
+  des: { type: String },
+  image: { type: String },
   isDelete: { type: Boolean },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
 
-
-module.exports = mongoose.model('Course',Course)
+module.exports = mongoose.model("Course", Course);
