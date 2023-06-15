a594ed171dfca346e90970281d2664c2a26d9135
TuanAn <tuananthptcc10a5@gmail.com>
2023-06-15 10:47:52 +0700

    add new books

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
+const slug = require('mongoose-slug-updater')
+
+mongoose.plugin(slug)
+
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
   name: { type: String },
   des: { type: String },
+  cost: {type:Number},
   image: { type: String },
+  slug:{type:String,slug:'name',unique:true} ,
   isDelete: { type: Boolean },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
